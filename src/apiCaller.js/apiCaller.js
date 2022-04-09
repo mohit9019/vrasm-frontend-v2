543a1f8cad72f30620139c1abb502f13e41bc439
mohit9019 <mohitchauhan2802@gmail.com>
2022-04-09 15:21:23 +0530

    small changes

--- src/apiCaller.js/apiCaller.js
+++ src/apiCaller.js/apiCaller.js
@@ -3,18 +3,18 @@ import Loader from "../html/other/Loader";
 class ApiCaller {
 
   static userData = {};
-
   open_access_apis = ['/template/get', '/user/login', '/user/forget_password', '/admin/get_users', '/buyer/register', '/buyer/otp'];
-
   site = 'http://localhost:4000/v1/';
+
   constructor() {
+    if (localStorage.getItem('userData') && localStorage.getItem('userData').length > 0) {
+      ApiCaller.userData = JSON.parse(localStorage.getItem('userData'));
+    }
   }
 
-
   postData({ url, data }) {
-
     // Loader.setLoader(1);
-    if(localStorage.getItem('userData') && localStorage.getItem('userData').length > 0){
+    if (localStorage.getItem('userData') && localStorage.getItem('userData').length > 0) {
       let userData = JSON.parse(localStorage.getItem('userData'));
       // data['user_id'] = userData.user_id;
       data['accesstoken'] = userData.accesstoken;
@@ -27,7 +27,7 @@ class ApiCaller {
           ApiCaller.userData = { ...data.data.data };
           localStorage.setItem('userData', JSON.stringify(data.data.data));
         }
-        if(data.data.status_code == '404'){
+        if (data.data.status_code == '404') {
           localStorage.clear();
           // redirect to login page.
         }
